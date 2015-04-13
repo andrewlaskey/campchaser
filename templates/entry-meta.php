3473f4feb60bda3996a3bda25e63a288588a4f11
Andrew Laskey <andrew.laskey@gmail.com>
2015-04-13 16:09:40 -0700

    added basic single post structure

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,2 +1,13 @@
-<time class="updated" datetime="<?= get_the_time('c'); ?>"><?= get_the_date(); ?></time>
-<p class="byline author vcard"><?= __('By', 'sage'); ?> <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?= get_the_author(); ?></a></p>
+<div class="post-sidebar">
+  <button class="save-site"># Likes</button>
+  <dl>
+    <dt>Location</dt>
+    <dd>XX.XXX, XX.XXX</dd>
+    <dt>Occupancy</dt>
+    <dd>5 - 8 people</dd>
+    <dt>Updated</dt>
+    <dd><time class="updated" datetime="<?= get_the_time('c'); ?>"><?= get_the_date(); ?></time></dd>
+    <dt>Added By</dt>
+    <dd class="byline author vcard"><?= __('By', 'sage'); ?> <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?= get_the_author(); ?></a></dd>
+  </dl>
+</div>
