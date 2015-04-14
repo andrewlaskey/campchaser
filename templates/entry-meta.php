fb7815756d68fe412cd7d0b2c36b1fce8f456b68
Andrew Laskey <andrew.laskey@gmail.com>
2015-04-14 14:06:15 -0700

    added custom field data post sidebar

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,10 +1,10 @@
 <div class="post-sidebar">
-  <button class="save-site"># Likes</button>
+  <button class="save-site"><?php the_field('likes'); ?> Likes</button>
   <dl>
     <dt>Location</dt>
-    <dd>XX.XXX, XX.XXX</dd>
+    <dd><?php the_field('location_coords'); ?></dd>
     <dt>Occupancy</dt>
-    <dd>5 - 8 people</dd>
+    <dd><?php the_field('occupancy'); ?></dd>
     <dt>Updated</dt>
     <dd><time class="updated" datetime="<?= get_the_time('c'); ?>"><?= get_the_date(); ?></time></dd>
     <dt>Added By</dt>
