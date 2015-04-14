f018d30cb4a0604251bb4604c7ad17b71b47e68e
Andrew Laskey <andrew.laskey@gmail.com>
2015-04-13 17:22:39 -0700

    added search query to page header

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,14 +1,18 @@
 <?php use Roots\Sage\Titles; ?>
 
 <div class="page-header">
-	<select class="post-select">
-		<option>Popular</option>
-		<option>Recent</option>
-		<?php if (is_user_logged_in()): ?>
-		<option>Your Sites</option>
-		<option>Saved Sites</option>
-		<?php endif; ?>
-	</select>
-	<button class="pag-header-item is-active js-toggle-list">List</button>
-	<button class="pag-header-item js-map-toggle">Map</button>
+  <?php if (is_search()): ?>
+  <h2><?php get_search_query() ?>
+  <?php else : ?>
+  <select class="post-select">
+    <option>Popular</option>
+    <option>Recent</option>
+    <?php if (is_user_logged_in()): ?>
+    <option>Your Sites</option>
+    <option>Saved Sites</option>
+    <?php endif; ?>
+  </select>
+  <?php endif; ?>
+  <button class="pag-header-item is-active js-toggle-list">List</button>
+  <button class="pag-header-item js-map-toggle">Map</button>
 </div>
